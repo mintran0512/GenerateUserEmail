2b098e82fac2bb18f98259c69724330712e3362c
mintran0512 <mtran@clango.com>
2017-04-19 15:40:27 -0500

    add java class and oimClient.jar file

---
+++ GenerateUserEmail/src/generateuseremail/GenerateUserEmail.java
@@ -0,0 +1,12 @@
+package generateuseremail;
+
+import java.util.HashSet;
+import javax.security.auth.login.LoginException;
+
+
+
+public class GenerateUserEmail {
+    public GenerateUserEmail() {
+        super();
+    }
+}
